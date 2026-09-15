695b665f2aa6d67a0a8941c9bf597e6581ffb792
cmulac <cgmulac@gmail.com>
2026-09-15 13:56:35 -0500

    Update hero.py

--- hero.py
+++ hero.py
@@ -7,12 +7,13 @@ class Hero:
         self.health = 125
         self.attack_power = 10
         self.armor = 3
+        self.criticalHit = 5
 
     def attack(self):
         return random.randint(1, self.attack_power)
 
     def take_damage(self, damage):
-        damage = damage - self.armor
+        damage = damage - self.armor + random.randint(0, self.criticalHit)
         self.health = max(0, self.health - damage)
         print(f"{self.name} takes {damage} damage. Health: {self.health}")
 
